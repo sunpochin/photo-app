9325d2e04af3968bc933167d5670cc25370eaa29
sun pochin <sunpochin@gmail.com>
2021-11-18 16:21:34 +0800

    adding logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # https://stackoverflow.com/questions/6567863/no-route-matches-get-users-sign-out  
   devise_for :users
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  
 #  get 'welcome/index'
   root 'welcome#index'
 
