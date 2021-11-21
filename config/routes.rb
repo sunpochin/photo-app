a7f0466d1286c0a147df949f8f82f005d31b0c72
sun pochin <sunpochin@gmail.com>
2021-11-21 20:01:14 +0800

    adding registrations data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # https://stackoverflow.com/questions/6567863/no-route-matches-get-users-sign-out  
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   devise_scope :user do  
      get '/users/sign_out' => 'devise/sessions#destroy'     
   end
